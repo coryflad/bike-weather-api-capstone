7cd299b0994319dc077a80846ad68122cd234af8
Cory Fladhammer <cjfladham@hotmail.com>
2020-01-04 12:44:58 -0600

    updated script.js with fetch

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const searchURL = "https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=be67025be3ad63215702c8c628cc2f8c"
+const apiWeatherKey = 'be67025be3ad63215702c8c628cc2f8c';
+const searchWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const apiYoutubeKey = 'AIzaSyAAgGwFpTIwbsW533bh_VcsHCeRgtexxKw';
+const searchYoutubeURL = 'https://www.googleapis.com/youtube/v3/search';
 
 // function used to convert wind direction data from degrees 
 //to cardinal / intercardinal directions
@@ -10,96 +14,122 @@ function degToCompass(num) {
     return arr[(val % 16)];
 }
 
-//retrieve data from OpenWeather API
-function getWeatherData(city) {
-    $.ajax(searchURL, {
-        data: {
-            units: 'imperial',
-            q: city
-        },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: function (data) {
-            let widget = displayWeather(data);
-            $('#weather-display').html(widget);
-        }
-    });
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
 }
 
-//retrieve data from YouTube API
-function getYouTubeData(userSearchTerm) {
-    $.getJSON("https://www.googleapis.com/youtube/v3/search", {
-        part: "snippet", //Youtube API special parameter (please check documentation here https://developers.google.com/youtube/v3/docs/search/list)
-        maxResults: 3, //number of results per page
-        key: "AIzaSyAAgGwFpTIwbsW533bh_VcsHCeRgtexxKw",
-        q: userSearchTerm, //search query from the user
-        type: "video" //only return videos (no channels or playlists) so we can take the video ID and link it back to Youtube
-    },
-        function (receivedApiData) {
-            //show the json array received from the API call
-            console.log(receivedApiData);
-            // if there are no results it will show an error
-            if (receivedApiData.pageInfo.totalResults == 0) {
-                alert("No videos found!");
+//creates li and displays in html
+function displayWeatherResults(responseJson) {
+    console.log(responseJson);
+
+    $('#weather-results').append(
+        `<li>
+        <ul>Current Weather for ${responseJson.name}</ul>
+        <ul>Temperature:${responseJson.main.temp}&#8457</ul>
+        <ul>Wind Speed:${responseJson.wind.speed}MPH</ul>
+        <ul>Wind Direction:${degToCompass(responseJson.wind.deg)}</ul>
+        </li>`
+    )
+    
+};
+
+//creates li and displays in html
+function displayYoutubeResults(responseJson) {
+    console.log(responseJson);
+
+    // iterate through the items array
+    for (let i = 0; i < responseJson.items.length; i++) {
+
+        // for each video object in the items 
+        // array, add a list item to the results 
+        // list with the video title, description,
+        // and thumbnail
+        $('#youtube-results').append(
+            `<li><h3>${responseJson.items[i].snippet.title}</h3>
+        <p>${responseJson.items[i].snippet.description}</p>
+        <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+        </li>`
+        )
+    };
+}
+
+//sends call to openweather API
+function getWeather(query) {
+    const params = {
+        q: query,
+        APPID: apiWeatherKey,
+        units: 'imperial',
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchWeatherURL + '?' + queryString;
+
+    console.log(url);
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
             }
-            //if there are results, call the displaySearchResults
-            else {
-                displayYouTube(receivedApiData.items);
-            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            displayWeatherResults(responseJson);
+            getYouTubeVideos(query);
+        })
+        .catch(err => {
+            $('#weather-results').html();
+            $('#js-error-message').text(`${query} ${err.message}! Please re-enter city / town name`);
         });
 }
 
-// displays the results from OpenWeather API
-function displayWeather(data) {
-    return `
-    <div class="weather-results">
-        <p>${data.name}</p>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <p>${data.weather[0].main}</p>
-        <p>Temperature:${data.main.temp}&#8457</p>
-        <p>Wind Speed:${data.wind.speed} MPH</p>
-        <p>Wind Direction:${degToCompass(data.wind.deg)}</p>
-    </div>`;
-}
-
-// displays the results from YouTube API
-function displayYouTube(videosArray) {
-    //create an empty variable to store one LI for each one the results
-    var buildTheHtmlOutput = "";
-
-    $.each(videosArray, function (videosArrayKey, videosArrayValue) {
-        //create and populate one LI for each of the results ( "+=" means concatenate to the previous one)
-        buildTheHtmlOutput += "<li class='search-results'>";
-        buildTheHtmlOutput += "<a href='https://www.youtube.com/watch?v=" + videosArrayValue.id.videoId + "' target='_blank'>"; //taget blank is going to open the video in a new window
-        buildTheHtmlOutput += "<img class='video' src='" + videosArrayValue.snippet.thumbnails.high.url + "'/>"; //display video's thumbnail
-        buildTheHtmlOutput += "</a>";
-        buildTheHtmlOutput += "<p>" + videosArrayValue.snippet.title + "</p>"; //output vide title
-        buildTheHtmlOutput += "</li>";
-    });
-
-    //use the HTML output to show it in the index.html
-    $("#search-results ul").html(buildTheHtmlOutput);
-}
-
-function enterLocation() {
-    $('.search-form').submit(function (event) {
-        event.preventDefault();
-        let city = $('.search-query').val();
-        $('#weather-display').html("");
-        $('.weather-row').show();
-        getWeatherData(city);
-        getYouTubeData(city);
-    });
-}
-
-//autocomplete location name in user input field
-function activatePlacesSearch() {
-    let options = {
-        types: ['(regions)']
+//sends call to youtube API
+function getYouTubeVideos(query) {
+    const params = {
+        key: apiYoutubeKey,
+        q: query,
+        part: 'snippet',
+        maxResults: 1
     };
-    let input = document.getElementById('search-term');
-    let autocomplete = new google.maps.places.Autocomplete(input, options);
+    const queryString = formatQueryParams(params)
+    const url = searchYoutubeURL + '?' + queryString;
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayYoutubeResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
 }
+
+
+//get user input
+function enterLocation() {
+    
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        if (searchTerm == '') {
+            alert('please input city name');
+        }
+        else {
+            // if there are previous results, remove them
+            $('#weather-results').html('');
+            $('#js-error-message').html('');
+            $('#youtube-results').html('');
+            getWeather(searchTerm);
+            $('#weather-results').removeClass("hidden");
+        }
+
+    });
+}
+
+
 
 
 $(enterLocation);
